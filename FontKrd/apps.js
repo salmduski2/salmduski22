eea0382ce5b9d12e7bc22cfbe7bfae7dc4109020
DevSalm <108820856+salmduski2@users.noreply.github.com>
2023-04-13 07:24:58 +0200

    Add files via upload

---
+++ FontKrd/apps.js
@@ -0,0 +1,6 @@
+function vshack(){window.open("itms-services://?action=download-manifest&url=https://plist.tweakdoor.com/videostarold","_self")}
+function vspro(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/vspro.plist","_self")}
+function snapx(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/snapx.plist","_self")}
+function insta(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/insta.plist","_self")}
+function tiktoklrd(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/tiktoklrd.plist","_self")}
+function youtube(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
